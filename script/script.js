5c91cea28cc8a37a2032f0170a39ec5a74ab6bf5
Lucas Kennde <166322461+LucasKennde@users.noreply.github.com>
2024-09-07 14:27:04 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,11 +1,14 @@
 const formularioTask = document.querySelector('#formulario-task')
+
 const task = document.querySelector('#task')
+
 const lista = document.querySelector('.lista-tasks')
 const tasks = []
 
 formularioTask.addEventListener('submit', (e)=>{
     e.preventDefault();
     const taskId = formularioTask.getAttribute('data-id')
+
     !taskId? adicionarTask() : atualizarTask(taskId, task.value)
     
 
@@ -17,18 +20,32 @@ function gerarIdUnico() {
     return '_' + Math.random().toString(36).substr(2, 9);
 }
 
+function verificarInput(){
+    if(task.value === ''){
+       alert('Preencha o campo com uma tarefa')
+       return false
+    }
+    return true
+}
+
 function adicionarTask(){
-    
+    if(!verificarInput()){
+        return
+    }
     const newTask = {
         id:gerarIdUnico(),
         text: task.value,
         completado: false
     }
     tasks.push(newTask)
+
     mostrarTasks()
 }
 
 function atualizarTask(id, novoTexto) {
+    if(verificarInput()){
+        return
+    }
     const taskAtualizada = tasks.find(task => task.id == id)
     taskAtualizada.text = novoTexto
     formularioTask.removeAttribute('data-id');
@@ -66,7 +83,7 @@ function mostrarTasks() {
         <section class="concluir" onclick = "concluirTask('${task.id}')"></section>
         <span>${task.text}</span>
         <button class="btn-editar" onclick="editarTask('${task.id}')">Editar</button>
-        <button class="btn btn-excluir" onclick="excluirTask('${task.id}')">Excluir</button>
+        <button class="btn-excluir" onclick="excluirTask('${task.id}')">Excluir</button>
         `
         lista.appendChild(tarefa)
     })
